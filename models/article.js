cd633c68591a19aa5bb5704012d2e0466af5b4cd
Charlene Gipulan <charlenegipulan@gmail.com>
2020-04-06 22:16:32 -0700

    create article model and schema

---
+++ models/article.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var articleSchema = mongoose.Schema({
+    title: {
+        type: String,
+        index: true,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        index: true,
+        required: true
+    },
+    date: {
+        type: date,
+        default: Date.now
+    }
+})
+
+var Article = module.exports = mongoose.model('Article', articleSchema);
