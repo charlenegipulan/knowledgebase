750bb18e27e33d585197b9a028d8787fa491dbb1
Charlene Gipulan <charlenegipulan@gmail.com>
2020-04-06 22:21:40 -0700

    add read methods in article model

--- models/article.js
+++ models/article.js
@@ -22,3 +22,19 @@ var articleSchema = mongoose.Schema({
 })
 
 var Article = module.exports = mongoose.model('Article', articleSchema);
+
+//get all articles
+module.exports.getArticles = function(callback){
+    Article.find(callback);
+}
+
+//get article by ID
+module.exports.getArticleById = function(id, callback) {
+    Article.findById(id, callback);
+}
+
+//get category articles
+module.exports.getArticleByCategory = function(category, callback) {
+    var query = {category: category};
+    Article.find(query, callback);
+}
